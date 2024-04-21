a4252633b4203708996e79abbc261c088f958347
Zulaikha <zhanif02@gmail.com>
2024-04-21 17:15:29 +0100

    linked both pages

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ document.body.classList += " menu--open"
 function closeMenu(){
     document.body.classList.remove('menu--open')  
 }
+
+function navigateToPage() {
+    window.location.href = 'books.html';
+}
+
+function navigateHome() {
+    window.location.href = 'index.html';
+}
